a231aff34fa1738eb2928cb9676c2f15e34cc125
Aman <145094500+iaman-g@users.noreply.github.com>
2024-05-16 19:17:01 +0530

    Update constants.ts

--- src/setup/constants.ts
+++ src/setup/constants.ts
@@ -1,6 +1,6 @@
 export const APP_VERSION = import.meta.env.PACKAGE_VERSION;
-export const DISCORD_LINK = "#";
-export const GITHUB_LINK = "https://github.com/iaman-g/binge/";
-export const DONATION_LINK = "#";
+export const DISCORD_LINK = "https://github.com/iaman-g/binge";
+export const GITHUB_LINK = "https://github.com/iaman-g/binge";
+export const DONATION_LINK = "https://github.com/iaman-g/binge";
 export const GA_ID = import.meta.env.VITE_GA_ID;
 export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
