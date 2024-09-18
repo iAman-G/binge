6261c3df7910bc97bda52e6c8732ebe7db6fb472
Aman <145094500+iAman-G@users.noreply.github.com>
2024-09-19 02:37:50 +0530

    Update captions.ts

--- src/components/player/utils/captions.ts
+++ src/components/player/utils/captions.ts
@@ -1,4 +1,4 @@
-import { RunOutput } from "@movie-web/providers";
+import { RunOutput } from "cinehub-providers";
 import DOMPurify from "dompurify";
 import { convert, detect, parse } from "subsrt-ts";
 import { ContentCaption } from "subsrt-ts/dist/types/handler";
